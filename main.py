c2d89dbb93fd91dd3e6c70ad489c312101d8758a
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-05-11 08:01:56 +0200

    mse version 1.0

--- main.py
+++ main.py
@@ -6,20 +6,25 @@ import numpy
 from sklearn import datasets
 
 
-def new_nn(n_input, n_output, n_hidden, n_hidden_layer, randomize):
+def new_nn(n_features, n_output, n_hidden_list, randomize):
     network = list()
-    for j in range(n_hidden_layer):
-        hidden_layer = [{'weights': [random() if randomize else 0 for i in range(n_input + 1)]} for i in range(n_hidden)]
+    n_hidden_layer = len(n_hidden_list)
+    n_hidden = n_hidden_list[0]
+    hidden_layer = [{'weights': [random() if randomize else 0 for i in range(n_features + 1)]} for i in range(n_hidden)]
+    network.append(hidden_layer)
+    for j in range(1, n_hidden_layer):
+        n_hidden = n_hidden_list[j]
+        hidden_layer = [{'weights': [random() if randomize else 0 for i in range(n_hidden_list[j-1] + 1)]} for i in range(n_hidden)]
         network.append(hidden_layer)
-    output_layer = [{'weights': [random() if randomize else 0 for i in range(n_hidden + 1)]} for i in range(n_output)]
+    output_layer = [{'weights': [random() if randomize else 0 for i in range(n_hidden_list[n_hidden_layer-1] + 1)]} for i in range(n_output)]
     network.append(output_layer)
     return network
 
 
 def activate(sample, weights):
     activation = weights[len(weights)-1]
-    for i in range(1, len(weights) - 1):
-        activation += weights[i] * sample[i-1]
+    for i in range(0, len(weights)-1):
+        activation += weights[i] * sample[i]
     return activation
 
 
@@ -32,13 +37,13 @@ def forward_propagate(neural_network, sample, function):
             neuron['output'] = transfer(activation, function)
             new_inputs.append(neuron['output'])
         inputs = new_inputs
-    n_input = len(inputs)
-    sum = 0
-    for input in inputs:
-        sum += input
-    if sum != 0:
-        for i in range(n_input):
-            inputs[i] = inputs[i] / sum
+    # n_input = len(inputs)
+    # sum = 0
+    # for input in inputs:
+    #     sum += input
+    # if sum != 0:
+    #     for i in range(n_input):
+    #         inputs[i] = inputs[i] / sum
     return inputs
 
 
@@ -83,7 +88,7 @@ def backpropagation(network, expected, derivative_function):
         else:
             for j in range(len(layer)):
                 neuron = layer[j]
-                errors.append(neuron['output'] - expected[j])
+                errors.append(neuron['output'] - expected)
         for j in range(len(layer)):
             neuron = layer[j]
             neuron['correction'] = errors[j] * transfer_derivative(neuron['output'], derivative_function)
@@ -96,17 +101,14 @@ def train_network(neural_network, training_dataset, learning_rate, decadence,
         prev_error = error
         error = 0
         for sample in training_dataset:
-            outputs = forward_propagate(neural_network, sample, function)
+            predicted = forward_propagate(neural_network, sample, function)
             # print(outputs)
-            expected = [0 for i in range(n_output)]
-            expected[sample[len(sample)-1]] = 1
-            for i in range(n_output):
-                # error += math.pow((expected[i]-outputs[i]), 2)
-                # error += -(expected[i]*math.log(outputs[i], math.e) + (1-expected[i])*math.log(1-outputs[i], math.e))
-                error += bce(expected[i], outputs[i])
+            expected = sample[len(sample)-1]
+            error += math.pow((expected-predicted), 2)
+            # error += bce(expected, predicted)
             backpropagation(neural_network, expected, derivative_function)
             update_weights(neural_network, sample, learning_rate / (decadence * (epoch+1)))
-        error /= (len(training_dataset)*n_output)
+        error /= (len(training_dataset))
         # print('epoch: ' + str(epoch) + ' learning rate: ' + str(round(learning_rate, precision))
         #       + ' error: ' + str(round(error, precision)))
         if epoch != 0 and error > prev_error:
@@ -143,7 +145,7 @@ def update_weights(neural_network, sample, learning_rate):
 
 def predict(neural_network, sample, activation_function):
     predicted = forward_propagate(neural_network, sample, activation_function)
-    return predicted.index(max(predicted))
+    return predicted
 
 
 def main():
@@ -152,7 +154,7 @@ def main():
     n_features = 4
     dataset = iris.data[:, :n_features]
     labels = iris.target
-    learning_rate = 0.001
+    learning_rate = 0.0001
     precision = 3
     decadence = 1 + learning_rate
     n_max_epoch = 1000
@@ -188,93 +190,101 @@ def main():
                 normalized_dataset[i][j] /= std_devs[j]
 
     # training and test sets definition
-    training_dataset = numpy.concatenate((numpy.concatenate((normalized_dataset[0:40], normalized_dataset[50:90]),
+    tmp_training_dataset = numpy.concatenate((numpy.concatenate((normalized_dataset[0:40], normalized_dataset[50:90]),
                                                             axis=0), normalized_dataset[100:140]), axis=0)
-    n_training = len(training_dataset)
-    training_dataset_labels = numpy.concatenate((numpy.concatenate((labels[0:40], labels[50:90]),
+    n_training = len(tmp_training_dataset)
+    tmp_training_dataset_labels = numpy.concatenate((numpy.concatenate((labels[0:40], labels[50:90]),
                                                                    axis=0), labels[100:140]), axis=0)
-    test_dataset = numpy.concatenate((numpy.concatenate((normalized_dataset[40:50], normalized_dataset[90:100]),
+    tmp_test_dataset = numpy.concatenate((numpy.concatenate((normalized_dataset[40:50], normalized_dataset[90:100]),
                                                         axis=0), normalized_dataset[140:150]), axis=0)
-    n_test = len(test_dataset)
-    test_dataset_labels = numpy.concatenate((numpy.concatenate((labels[40:50], labels[90:100]),
+    n_test = len(tmp_test_dataset)
+    tmp_test_dataset_labels = numpy.concatenate((numpy.concatenate((labels[40:50], labels[90:100]),
                                                                axis=0), labels[140:150]), axis=0)
 
-    tmp_training_dataset = []
+    training_dataset = []
     for i in range(0, n_training):
-        tmp_sample = []
+        sample = []
         for j in range(0, n_features):
-            tmp_sample.insert(j, training_dataset[i][j])
-        tmp_sample.insert(n_features, training_dataset_labels[i])
-        tmp_training_dataset.insert(i, tmp_sample)
+            sample.insert(j, tmp_training_dataset[i][j])
+        sample.insert(n_features, tmp_training_dataset_labels[i])
+        training_dataset.insert(i, sample)
 
-    tmp_test_dataset = []
+    test_dataset = []
     for i in range(0, n_test):
-        tmp_sample = []
+        sample = []
         for j in range(0, n_features):
-            tmp_sample.insert(j, test_dataset[i][j])
-        tmp_sample.insert(n_features, test_dataset_labels[i])
-        tmp_test_dataset.insert(i, tmp_sample)
+            sample.insert(j, tmp_test_dataset[i][j])
+        sample.insert(n_features, tmp_test_dataset_labels[i])
+        test_dataset.insert(i, sample)
 
-    training_dataset1 = copy.deepcopy(tmp_training_dataset)
-    for i in range(0, n_training):  # 010
-        if training_dataset1[i][n_features] == 2:
-            training_dataset1[i][n_features] = 0
-    test_dataset1 = copy.deepcopy(tmp_test_dataset)
-    for i in range(0, n_test):  # 010
-        if test_dataset1[i][n_features] == 2:
-            test_dataset1[i][n_features] = 0
+    seed(1)
+    n_output = 1
+    network = new_nn(n_features, n_output, [5, 5, 5, 5], True)
+    train_network(network, training_dataset, learning_rate, decadence, n_max_epoch, n_output,
+                  relu, relu_derivative, precision)
+    # for layer in network:
+    #     for neuron in layer:
+    #         print(neuron['weights'])
+    #     print()
 
-    training_dataset2 = copy.deepcopy(tmp_training_dataset)
-    for i in range(0, n_training):  # 100
-        if training_dataset2[i][n_features] == 0:
-            training_dataset2[i][n_features] = 1
+    min_0, min_1, min_2 = 1, 1, 1
+    max_0, max_1, max_2 = 0, 0, 0
+    mean_0, mean_1, mean_2 = 0, 0, 0
+    n_0, n_1, n_2 = 0, 0, 0
+    for i in range(0, n_training):
+        sample = training_dataset[i]
+        expected = training_dataset[i][len(sample)-1]
+        predicted = predict(network, sample, relu)[0]
+        print("expected: " + str(expected) + " predicted: " + str(predicted))
+        if expected == 0:
+            mean_0 += predicted
+            n_0 += 1
+            if predicted > max_0:
+                max_0 = predicted
+            if predicted < min_0:
+                min_0 = predicted
+        elif expected == 1:
+            mean_1 += predicted
+            n_1 += 1
+            if predicted > max_1:
+                max_1 = predicted
+            if predicted < min_1:
+                min_1 = predicted
         else:
-            training_dataset2[i][n_features] = 0
-    test_dataset2 = copy.deepcopy(tmp_test_dataset)
-    for i in range(0, n_test):  # 100
-        if test_dataset2[i][n_features] == 0:
-            test_dataset2[i][n_features] = 1
+            mean_2 += predicted
+            n_2 += 1
+            if predicted > max_2:
+                max_2 = predicted
+            if predicted < min_2:
+                min_2 = predicted
+    mean_0, mean_1, mean_2 = mean_0 / n_0, mean_1 / n_1, mean_2 / n_2
+    threshold_1, threshold_2 = (max_0 + min_1) / 2, (max_1 + min_2) / 2
+
+    print("labels 0's mean: " + str(round(mean_0, precision)) + ", labels 1's mean: " +
+          str(round(mean_1, precision)) + ", labels 2's mean: " + str(round(mean_2, precision)) +
+          ", \nlabels 0's max: " + str(round(max_0, precision))
+          + ", labels 1's max: " + str(round(max_1, precision)) + ",labels 2's max: " +
+          str(round(max_2, precision)) + ", \nlabels 0's min: " + str(round(min_0, precision)) +
+          ", labels 1's min: " + str(round(min_1, precision)) + ", labels 2's min: "
+          + str(round(min_2, precision)) + ", threshold_1: "
+          + str(round(threshold_1, precision)) + ", threshold_2: " + str(round(threshold_2, precision)) + ".\n")
+
+    accuracy = 0
+    for sample in test_dataset:
+        expected = sample[len(sample)-1]
+        predicted = predict(network, sample, relu)[0]
+        if predicted < threshold_1:
+            predicted = 0
+        elif predicted < threshold_2:
+            predicted = 1
         else:
-            test_dataset2[i][n_features] = 0
-
-    training_dataset3 = copy.deepcopy(tmp_training_dataset)
-    for i in range(0, n_training):  # 001
-        if training_dataset3[i][n_features] == 2:
-            training_dataset3[i][n_features] = 1
-        else:
-            training_dataset3[i][n_features] = 0
-    test_dataset3 = copy.deepcopy(tmp_test_dataset)
-    for i in range(0, n_test):  # 001
-        if test_dataset3[i][n_features] == 2:
-            test_dataset3[i][n_features] = 1
-        else:
-            test_dataset3[i][n_features] = 0
-
-    training_datasets = [training_dataset1, training_dataset2, training_dataset3]
-    test_datasets = [test_dataset1, test_dataset2, test_dataset3]
-
-    for i in range(0, 3):
-        seed(1)
-        n_input = n_features
-        n_output = 2
-        network = new_nn(n_input, n_output, 4, 2, True)
-        train_network(network, training_datasets[i], learning_rate, decadence, n_max_epoch, n_output,
-                      relu, relu_derivative, precision)
-        # for layer in network:
-        #     for neuron in layer:
-        #         print(neuron['weights'])
-        #     print()
-
-        accuracy = 0
-        for sample in test_datasets[i]:
-            expected = sample[len(sample)-1]
-            predicted = predict(network, sample, relu)
-            if expected == predicted:
-                accuracy += 1
-            print('expected: ' + str(round(expected, precision)) + ' predicted: ' + str(round(predicted, precision)))
-        accuracy /= n_test
-        accuracy *= 100
-        print("accuracy: " + str(round(accuracy, precision)))
+            predicted = 2
+        if expected == predicted:
+            accuracy += 1
+        print('expected: ' + str(round(expected, precision)) + ' predicted: ' + str(round(predicted, precision)))
+    accuracy /= n_test
+    accuracy *= 100
+    print("accuracy: " + str(round(accuracy, precision)))
 
 
 main()
